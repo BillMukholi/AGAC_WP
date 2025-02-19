1746d841d84f32a8197ad587179814092a0e3bd3
Bill Mukholi <work.billmukholi@gmail.com>
2025-02-19 13:25:39 +0300

    Linked CSS & JS

--- functions.php
+++ functions.php
@@ -140,8 +140,11 @@ add_action( 'widgets_init', 'agac_widgets_init' );
 function agac_scripts() {
 	wp_enqueue_style( 'agac-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'agac-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'agac-css', get_template_directory_uri().'/css/agac.css', array(), _S_VERSION );
+
 
 	wp_enqueue_script( 'agac-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'agac-js', get_template_directory_uri() . '/js/agac.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
